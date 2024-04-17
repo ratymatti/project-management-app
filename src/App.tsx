4fe06a566f0b252e530d6b5c7bf11ffc46875ba5
Matti Räty <mattiraty@hotmail.com>
2024-04-17 15:30:41 +0300

    transformed project to typescript

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <h1 className="my-8 text-center text-5xl font-bold">project manager</h1>
+    </>
+  );
+}
+
+export default App;
