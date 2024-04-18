14af1c1495b6d04ccdebbc6a5de17a7b4efec4d6
Matti Räty <mattiraty@hotmail.com>
2024-04-18 15:01:33 +0300

    created basic structure for NewProject component

---
+++ src/components/NewProject.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Input from "./Input";
+
+export default function NewProject() {
+    return (
+        <div className="w-[35rem] mt-16">
+            <menu className="flex items-center justify-end gap-4 my-4">
+                <li>
+                    <button className="text-stone-800 hover:text-stone950">Cancel</button>
+                </li>
+                <li>
+                    <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button>
+                </li>
+            </menu>
+
+            <div>
+                <Input label="Project Name" />
+                <Input label="Description" textarea />
+                <Input label="Due Date" type="date" />
+            </div>
+        </div>
+    )
+}
