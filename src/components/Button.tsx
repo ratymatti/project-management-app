021a616b973cdd4e8932a13db2d9e70aac80ffe1
Matti Räty <mattiraty@hotmail.com>
2024-04-18 15:02:16 +0300

    created re-usable button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+interface ButtonProps {
+    children: string
+    onClick: () => void
+}
+
+export default function Button({ children, ...props }: ButtonProps) {
+    const buttonStyles = "px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100";
+
+    return (
+        <button className={buttonStyles} {...props}>
+            {children}
+        </button>
+    )
+}
