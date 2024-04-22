4e361809aef5c3fc23a552287a14397c6484346d
Matti Räty <mattiraty@hotmail.com>
2024-04-22 12:56:08 +0300

    created context provider for projectsState

---
+++ src/contexts/ProjectsContext.tsx
@@ -0,0 +1,86 @@
+import React, { ReactNode, createContext, useState } from 'react'
+import { Project, ProjectID, ProjectsState } from '../types/project';
+
+import { initialProjectsState } from '../utils/initialProjectsState'
+
+export interface ProjectsContextType {
+    projectsState: ProjectsState;
+    handleStartAddProject: () => void;
+    handleAddProject: (newProject: Project) => void;
+    handleCancelAddProject: () => void;
+    handleSelectProject: (projectId: ProjectID) => void;
+    handleDeleteProject: (projectId: ProjectID) => void;
+}
+
+export const ProjectsContext = createContext<ProjectsContextType | undefined>(undefined);
+
+export function ProjectsProvider({ children }: { children: ReactNode }): JSX.Element {
+    const [projectsState, setProjectsState] = useState<ProjectsState>({ ...initialProjectsState });
+
+    function handleStartAddProject(): void {
+        setProjectsState((prevState: ProjectsState): ProjectsState => {
+            return {
+                ...prevState,
+                selectedProjectID: null, // renders NewProject
+                selectedProject: null // clears SelectedProject
+            }
+        });
+    }
+
+    function handleAddProject(newProject: Project): void {
+        setProjectsState((prevState: ProjectsState): ProjectsState => {
+            return {
+                ...prevState,
+                projects: [...prevState.projects, newProject],
+                selectedProjectID: newProject.id,
+                selectedProject: newProject // renders SelectedProject with newProject
+            }
+        });
+    }
+
+    function handleCancelAddProject(): void {
+        setProjectsState((prevState: ProjectsState): ProjectsState => {
+            return {
+                ...prevState,
+                selectedProjectID: undefined // renders NoProjectSelected
+            }
+        });
+    }
+
+    function handleSelectProject(projectId: ProjectID): void {
+        const project = projectsState.projects.find(project => project.id === projectId);
+
+        setProjectsState((prevState: ProjectsState): ProjectsState => {
+            return {
+                ...prevState,
+                selectedProjectID: projectId,
+                selectedProject: project! // renders SelectedProject with project
+            }
+        });
+    }
+
+    function handleDeleteProject(projectId: ProjectID): void {
+        setProjectsState((prevState: ProjectsState): ProjectsState => {
+            const updatedProjects = prevState.projects.filter(project => project.id !== projectId);
+            return {
+                ...prevState,
+                projects: updatedProjects,
+                selectedProjectID: undefined, // renders NoProjectSelected
+                selectedProject: null // clears SelectedProject
+            }
+        });
+    }
+
+    return (
+        <ProjectsContext.Provider value={{
+            projectsState,
+            handleStartAddProject,
+            handleAddProject,
+            handleCancelAddProject,
+            handleSelectProject,
+            handleDeleteProject
+        }}>
+            {children}
+        </ProjectsContext.Provider>
+    )
+}
