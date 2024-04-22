b4ffecd3050ea4743656aaf36937c34757d3c99b
Matti Räty <mattiraty@hotmail.com>
2024-04-22 17:51:49 +0300

    added styling

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -39,7 +39,7 @@ export default function Tasks({ project, updateProjectTasks }: TasksProps) {
     }
 
     return (
-        <section>
+        <section className='pb-10'>
             <h2 className='text-2xl font-bold text-stone-700 mb-4'>Tasks</h2>
             <NewTask
                 onAddTask={handleAddNewTask} />
