0ae05c5787373e79a792bb37688301a46fa57600
Matti Räty <mattiraty@hotmail.com>
2024-06-17 18:09:13 +0300

    changed to only render input elements

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,24 +1,25 @@
-import React, { forwardRef, useImperativeHandle, useRef, ChangeEvent } from "react"
+import React, {
+    forwardRef,
+    useImperativeHandle,
+    useRef,
+    ChangeEvent
+} from "react"
 
 interface InputProps {
     label?: string
-    textarea?: boolean
     type?: string
     placeholder?: string
     onChange?: (event: ChangeEvent<HTMLInputElement>) => void
     value?: string
 }
 
-type Ref = HTMLInputElement | HTMLTextAreaElement;
+type Ref = HTMLInputElement;
 
-const Input = forwardRef<Ref, InputProps>(function Input({ onChange, value, label, textarea, type = 'text', ...props }, ref) {
+const Input = forwardRef<Ref, InputProps>(function Input({ onChange, value, label, type = 'text', ...props }, ref) {
     const inputRef = useRef<HTMLInputElement>(null);
-    const textareaRef = useRef<HTMLTextAreaElement>(null);
 
     useImperativeHandle(ref, () => {
-        if (textarea && textareaRef.current) {
-            return textareaRef.current;
-        } else if (!textarea && inputRef.current) {
+        if (inputRef.current) {
             return inputRef.current;
         }
         throw new Error('Ref is null');
@@ -29,18 +30,13 @@ const Input = forwardRef<Ref, InputProps>(function Input({ onChange, value, labe
     return (
         <p className="flex flex-col gap-1 my-4">
             <label className="text-sm font-bold uppercase text-stone-500">{label}</label>
-            {textarea ?
-                <textarea
-                    ref={textareaRef}
-                    className={styles}
-                    {...props} />
-                : <input
-                    ref={inputRef}
-                    onChange={onChange}
-                    value={value}
-                    className={styles}
-                    type={type}
-                    {...props} />}
+            <input
+                ref={inputRef}
+                onChange={onChange}
+                value={value}
+                className={styles}
+                type={type}
+                {...props} />
         </p>
     )
 })
